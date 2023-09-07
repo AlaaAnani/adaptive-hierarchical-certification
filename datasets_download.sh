95f714608b6387d0c10d63b40f4915e3c8fec81f
AlaaAnani <alaa.a.anani@aucegypt.edu>
2023-09-07 12:47:15 +0200

    ignore datasets

---
+++ datasets_download.sh
@@ -0,0 +1,14 @@
+
+
+mkdir data/cityscapes/
+cd data/cityscapes/
+wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=aanani@mpi-sws.org&password=mypassword=Login' https://www.cityscapes-dataset.com/login/
+# refer to https://github.com/cemsaz/city-scapes-script
+# get gtFine data/gtFine_trainvaltest.zip
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
+unzip gtFine_trainvaltest.zip
+rm -rf gtFine_trainvaltest.zip
+# get  leftImg8bit data/leftImg8bit_trainvaltest.zip
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
+unzip leftImg8bit_trainvaltest.zip
+rm -rf leftImg8bit_trainvaltest.zip
